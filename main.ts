f1f1e496ebc12f6b6c60e634e65e860ef6b4c0a7
ella-an2 <ella.an@mths.ca>
2024-10-29 11:05:21 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella An
+ * Created on: Oct 2024
+ * This program counts down to 4
 */
 
-basic.showString('Hello, World!')
+// variables
+let loopCounter = 4
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// button a pressed
+input.onButtonPressed(Button.A, function () { 
+    while (loopCounter > 0) {
+        basic.showNumber(loopCounter)
+        basic.pause(500)
+        loopCounter = loopCounter - 1
+    }
+})
