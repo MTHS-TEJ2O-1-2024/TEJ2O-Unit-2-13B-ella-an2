da69314a8c9809566a6b4caa0994b57d5e20d363
ella-an2 <ella.an@mths.ca>
2024-12-09 16:03:05 +0000

    started coding

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,17 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import *
 
+loop_counter = 4
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+if button_a.is_pressed():
+    while loop_counter >= 0:
+        display.show(loop_counter)
+        sleep(1)
+
+        if loop_counter == 4:
+            
